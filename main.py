ec314a3d2d3ebb661cb43a42091f78b97361754d
209 <209@academicos.cesde.edu.co>
2022-08-13 13:07:08 -0500

    NTS2-Se crea nuevo proyecto de python, se conoce datos de entrada y salida y se crea archivo de reto1 con nuevos condicionales

---
+++ main.py
@@ -0,0 +1,38 @@
+#Comentario de linea
+
+#Salida por consola
+print("Hola mundo")
+print('Hola mundo')
+
+
+#Entradas
+#DATOS PRIMITIVOS
+dato = None
+nombre="Sebas"
+edad=20
+estatura=1.68
+boolean=True
+
+
+#salida por teclado
+print(dato," Hola")
+print(nombre)
+print(edad)
+print(estatura)
+print(boolean)
+print(f'hola {nombre} tu edad es {edad}')
+
+#recibir datos por teclado
+ciudad=input("Digite su ciudad de residencia: ")
+print(f'La ciudad digitada es {ciudad}')
+
+#recibir datos numericos por teclado
+numero1=int(input("Digite el numero 1: "))
+numero2=int(input("Digite el numero 2: "))
+print(numero1,numero2)
+
+#operadores aritmeticos
+
+#(+ - * / %)
+suma=numero1+numero2
+print(suma)
---
+++ reto1.py
@@ -0,0 +1,7 @@
+num=int(input("Digite el numero: "))
+mod=num%5
+if(mod==0):
+    print("Es multiplo de 5")
+else:
+    print("no es multiplo de 5")
+print("Fin del programa")
